5df79486707f89767234269f33bf62dc06ec24b9
youngil <kjk00999@gmail.com>
2025-08-21 10:31:29 +0900

    add Login.js

---
+++ Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+    
+    const Login = () => {
+        document.write("Login Success");
+    }
+
+    return (
+        <div>
+            {Login()}        
+        </div>
+    );
+};
+
+export default Login;
